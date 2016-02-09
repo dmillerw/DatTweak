ee9f42011f67ebb83cde60b9a38cee34d1ab0bfe
Dylan Miller <dmillerw@gmail.com>
2016-02-09 11:54:32 -0800

    Fixed sync packet handling

--- src/main/java/me/dmillerw/tweak/core/handler/NetworkEventHandler.java
+++ src/main/java/me/dmillerw/tweak/core/handler/NetworkEventHandler.java
@@ -1,8 +1,6 @@
 package me.dmillerw.tweak.core.handler;
 
-import me.dmillerw.tweak.core.network.PacketHandler;
 import me.dmillerw.tweak.core.network.message.MessageServerConfig;
-import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent;
 
@@ -13,9 +11,11 @@ public class NetworkEventHandler {
 
     @SubscribeEvent
     public void onPlayerLogin(PlayerEvent.PlayerLoggedInEvent event) {
-        // Send config packet from server
-        PacketHandler.INSTANCE.sendTo(new MessageServerConfig(), (EntityPlayerMP) event.player);
+        MessageServerConfig.sendServerConfig(event.player);
     }
 
-
+    @SubscribeEvent
+    public void onPlayerLogoff(PlayerEvent.PlayerLoggedOutEvent event) {
+        MessageServerConfig.sendReset(event.player);
+    }
 }
